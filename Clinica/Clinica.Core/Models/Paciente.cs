1f629d45db9512b79e5b6617e503cf3f7301c30e
Ernesto Abeyta <neto_930@hotmail.com>
2019-03-25 16:38:02 -0600

    Creación de persistencia en el cache y metodos CRUD en listas locales

---
+++ Clinica/Clinica.Core/Models/Paciente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinica.Core.Models
+{
+    public class Paciente
+    {
+
+        
+        public string Id { get; set; }
+        [StringLength(20)]
+        [DisplayName("Nombre del Paciente")]
+        public string Nombre { get; set; }
+        public string Edad { get; set; }
+        public string FechaDeNacimiento { get; set; }
+        public string Sexo { get; set; }
+        public string peso { get; set; }
+
+        public Paciente()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+    }
+
+    
+}
---
+++ Clinica/Clinica.DataAccess.InMemory/RepositorioPaciente.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using Clinica.Core.Models;
+
+namespace Clinica.DataAccess.InMemory
+{
+    public class RepositorioPaciente
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Paciente> pacientes;
+
+        public RepositorioPaciente()
+        {
+            pacientes = cache["pacientes"] as List<Paciente>;
+            if (pacientes == null)
+            {
+                pacientes = new List<Paciente>();
+            }
+        }
+
+        public void Guardar()
+        {
+            cache["pacientes"] = pacientes; 
+        }
+
+        public void AgregaPaciente(Paciente p)
+        {
+            pacientes.Add(p);
+        }
+
+        public void EditaPaciente (Paciente paciente)
+        {
+            Paciente PacienteAcualizar = pacientes.Find(p => p.Id == paciente.Id);
+            if(PacienteAcualizar != null)
+            {
+                PacienteAcualizar = paciente;
+            }
+            else{
+                throw new Exception("Paciente no encontrado");
+            }
+        }
+
+        public Paciente Buscar(string Id)
+        {
+            Paciente paciente = pacientes.Find(p => p.Id == Id);
+            if (paciente != null)
+            {
+                return paciente;
+            }
+            else
+            {
+                throw new Exception("Paciente no encontrado");
+            }
+        }
+
+        public IQueryable<Paciente> Pacientes()
+        {
+            return pacientes.AsQueryable();
+        }
+
+        public void EliminaPaciente(string Id)
+        {
+            Paciente PacienteEliminar = pacientes.Find(p => p.Id == Id);
+            if (PacienteEliminar != null)
+            {
+                pacientes.Remove(PacienteEliminar);
+            }
+            else
+            {
+                throw new Exception("Paciente Eliminado");
+            }
+        }
+    }
+}
+ 
